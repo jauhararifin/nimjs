12867ab7fc27af7d2b68001a012c5434799a846d
Jauhar Arifin <jauhararifin10@gmail.com>
2018-06-09 06:45:26 +0700

    fix tab size

--- crawler/crawler.ts
+++ crawler/crawler.ts
@@ -4,28 +4,28 @@ import { faculties } from './faculties';
 import { majors } from './majors';
 
 export interface Student extends nicutil.Student {
-	facultyCode: string;
-	majorCode?: string;
+  facultyCode: string;
+  majorCode?: string;
 }
 
 export interface Major {
-	code: string;
-	facultyCode: string;
-	name: string;
+  code: string;
+  facultyCode: string;
+  name: string;
 }
 
 export interface Faculty {
-	code: string;
-	name: string;
+  code: string;
+  name: string;
 }
 
 export interface Crawler {
 
-	crawlFaculties(): AsyncIterableIterator<Faculty>;
+  crawlFaculties(): AsyncIterableIterator<Faculty>;
 
-	crawlMajors(): AsyncIterableIterator<Major>;
+  crawlMajors(): AsyncIterableIterator<Major>;
 
-	crawlStudents(major: string, year: number): AsyncIterableIterator<Student>;
+  crawlStudents(major: string, year: number): AsyncIterableIterator<Student>;
 
 }
 
@@ -35,64 +35,64 @@ const MAX_RETRY_UNTIL_FAILED = 5;
 
 export class AbstractCrawler implements Crawler {
 
-    constructor(private nic: nicutil.Nic) {
-    }
+  constructor(private nic: nicutil.Nic) {
+  }
 
-    async *crawlFaculties(): AsyncIterableIterator<Faculty> {
-        for (const faculty of faculties) {
-            yield {...faculty};
-        } 
-    }
+  async *crawlFaculties(): AsyncIterableIterator<Faculty> {
+    for (const faculty of faculties) {
+      yield {...faculty};
+    } 
+  }
 
-    async *crawlMajors(): AsyncIterableIterator<Major> {
-        for (const major of majors) {
-            yield {...major};
+  async *crawlMajors(): AsyncIterableIterator<Major> {
+    for (const major of majors) {
+      yield {...major};
+    }
+  }
+
+  async *crawlStudents(major: string, year: number): AsyncIterableIterator<Student> {
+    let consecutiveFails = 0;
+
+    const yearStr = ("00" + year.toString()).slice(-2);
+    const prefix = major + yearStr;
+    const queue = new Array(700).map((val, i) => {
+      return {
+        keyword: prefix + ("000" + (i+1).toString()).slice(-3),
+        tried: 0,
+      };
+    });
+
+    while (queue.length > 0) {
+      if (consecutiveFails > MAX_CONSECUTIVE_FAIL_THRESHOLD) {
+        break;
+      }
+      
+      const { keyword, tried } = queue.shift();
+      if (tried > MAX_RETRY_UNTIL_FAILED) {
+        consecutiveFails++;
+        continue;
+      }
+      
+      try {
+        const student = await this.nic.checkStudent(keyword);
+        const result = {
+          ...student,
+          facultyCode: student.tpbNim.substr(0, 3),   
+        };
+        if (student.nim !== student.tpbNim) {
+          result['majorCode'] = student.nim.substr(0, 3);
         }
+        yield result;
+      } catch (e) {
+        queue.push({keyword, tried: tried + 1});
+      }
     }
-
-    async *crawlStudents(major: string, year: number): AsyncIterableIterator<Student> {
-        let consecutiveFails = 0;
-
-        const yearStr = ("00" + year.toString()).slice(-2);
-        const prefix = major + yearStr;
-        const queue = new Array(700).map((val, i) => {
-            return {
-                keyword: prefix + ("000" + (i+1).toString()).slice(-3),
-                tried: 0,
-            };
-        });
-
-        while (queue.length > 0) {
-            if (consecutiveFails > MAX_CONSECUTIVE_FAIL_THRESHOLD) {
-                break;
-            }
-            
-            const { keyword, tried } = queue.shift();
-            if (tried > MAX_RETRY_UNTIL_FAILED) {
-                consecutiveFails++;
-                continue;
-            }
-            
-            try {
-                const student = await this.nic.checkStudent(keyword);
-                const result = {
-                    ...student,
-                    facultyCode: student.tpbNim.substr(0, 3),   
-                };
-                if (student.nim !== student.tpbNim) {
-                    result['majorCode'] = student.nim.substr(0, 3);
-                }
-                yield result;
-            } catch (e) {
-                queue.push({keyword, tried: tried + 1});
-            }
-        }
-    }
+  }
 
 }
 
 export class NicCrawler extends AbstractCrawler {
-    constructor(username: string, password: string) {
-        super(new nicutil.StandardNic(username, password));
-    }
+  constructor(username: string, password: string) {
+    super(new nicutil.StandardNic(username, password));
+  }
 }
--- nicutil/nic.ts
+++ nicutil/nic.ts
@@ -7,98 +7,95 @@ const NIC_LOGIN_URL = "https://login.itb.ac.id/cas/login?service=https%3A%2F%2Fn
 const NIC_CHECK_STUDENT_URL = "https://nic.itb.ac.id/manajemen-akun/pengecekan-user";
 
 export interface Nic {
-    checkStudent(keyword: string): Promise<Student>;   
+  checkStudent(keyword: string): Promise<Student>;   
 }
 
 export class StandardNic implements Nic {
 
-    private agent: SuperAgent<SuperAgentRequest>;
+  private agent: SuperAgent<SuperAgentRequest>;
 
-    private hasLoggedIn = false;
+  private hasLoggedIn = false;
 
-    constructor(private username: string, private password: string) {
-        this.agent = agent();
+  constructor(private username: string, private password: string) {
+    this.agent = agent();
+  }
+
+  private async login() {
+    const response = await this.agent.get(NIC_LOGIN_URL);
+    const fields = this.fetchLoginFields(response.text);
+    fields['username'] = this.username;
+    fields['password'] = this.password;
+
+    await this.agent.post(NIC_LOGIN_URL).type('form').send(fields);
+  }
+
+  private fetchLoginFields(text: string): { [id: string]: string } {
+    const result = {'submit': 'LOGIN'};
+    const selector = load(text);
+    result['lt'] = selector("input[name='lt']").val();
+    result['execution'] = selector("input[name='execution']").val();
+    result['_eventId'] = selector("input[name='_eventId']").val();
+    return result;
+  }
+
+  async checkStudent(keyword: string): Promise<Student> {
+    if (!this.hasLoggedIn) {
+      await this.login();
+      this.hasLoggedIn = true;
     }
 
-    private async login() {
-        const response = await this.agent.get(NIC_LOGIN_URL);
-        const fields = this.fetchLoginFields(response.text);
-        fields['username'] = this.username;
-        fields['password'] = this.password;
-
-        await this.agent.post(NIC_LOGIN_URL).type('form').send(fields);
-    }
-
-    private fetchLoginFields(text: string): { [id: string]: string } {
-        const result = {'submit': 'LOGIN'};
-        const selector = load(text);
-        result['lt'] = selector("input[name='lt']").val();
-        result['execution'] = selector("input[name='execution']").val();
-        result['_eventId'] = selector("input[name='_eventId']").val();
-        return result;
-    }
-
-    async checkStudent(keyword: string): Promise<Student> {
-        if (!this.hasLoggedIn) {
-            await this.login();
-            this.hasLoggedIn = true;
-        }
-
-        let response: Response = undefined;
-        for (let i = 0; i < 5; i++) {
-            try {
-                response = await this.agent.post(NIC_CHECK_STUDENT_URL).type('form').send({
-                    'uid': keyword,
-                    'submit': ' Cari ',
-                });
-            } catch (e) {
-                await this.login();
-            }
-        }
-
-        if (!response) {
-            throw new Error('Error fetching student from nic');
-        }
-
-        const student = {
-            username: undefined,
-            name: undefined,
-            tpbNim: undefined,
-            email: undefined,
-            ai3Email: undefined,
-        };
-        const selector = load(response.text);
-        selector("td").map((i, element) => {
-            const value = element.firstChild.data;
-            if (value === undefined || value == null) {
-                return;
-            }
-            
-            if (i === 2) {
-                student.username = value.trim();
-            }
-            else if (i === 5) {
-                const nim = value.split(",").map(x => x.trim());
-                student.tpbNim = nim[0];
-                if (nim.length > 1) {
-                    student['nim'] = nim[1];
-                }
-            } else if (i === 8) {
-                student.name = value.trim();
-                   }
-            else if (i === 14) {
-                student.ai3Email = value.trim().split("(at)").join("@").split("(dot)").join(".");
-                 }
-            else if (i === 17) {
-                student.email = value.trim().split("(at)").join("@").split("(dot)").join(".");
-                 }
+    let response: Response = undefined;
+    for (let i = 0; i < 5; i++) {
+      try {
+        response = await this.agent.post(NIC_CHECK_STUDENT_URL).type('form').send({
+          'uid': keyword,
+          'submit': ' Cari ',
         });
-
-        if (!student.username) {
-            throw new Error('Student not found');
-        }
-
-        return student;
+      } catch (e) {
+        await this.login();
+      }
     }
+
+    if (!response) {
+      throw new Error('Error fetching student from nic');
+    }
+
+    const student = {
+      username: undefined,
+      name: undefined,
+      tpbNim: undefined,
+      email: undefined,
+      ai3Email: undefined,
+    };
+    const selector = load(response.text);
+    selector("td").map((i, element) => {
+      const value = element.firstChild.data;
+      if (value === undefined || value == null) {
+        return;
+      }
+      
+      if (i === 2) {
+        student.username = value.trim();
+      } else if (i === 5) {
+        const nim = value.split(",").map(x => x.trim());
+        student.tpbNim = nim[0];
+        if (nim.length > 1) {
+          student['nim'] = nim[1];
+        }
+      } else if (i === 8) {
+        student.name = value.trim();
+      } else if (i === 14) {
+        student.ai3Email = value.trim().split("(at)").join("@").split("(dot)").join(".");
+      } else if (i === 17) {
+        student.email = value.trim().split("(at)").join("@").split("(dot)").join(".");
+      }
+    });
+
+    if (!student.username) {
+      throw new Error('Student not found');
+    }
+
+    return student;
+  }
 
 }
