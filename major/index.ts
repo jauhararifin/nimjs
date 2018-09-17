888e891ceaa5b6067e66cd4c4eeeac8565f2b6d0
Jauhar Arifin <jauhararifin10@gmail.com>
2018-09-17 08:49:37 +0700

    fix major controller

--- major/index.ts
+++ major/index.ts
@@ -1,39 +1,34 @@
 
 import { Router, Request, Response, NextFunction } from 'express';
 import { Document, Types, Model } from 'mongoose';
-import { majors } from '../crawler/crawlerutil/majors';
-import { FacultyModel, MajorModel, StudentModel, createFacultyModel, createMajorModel, createStudentModel } from '../model';
-import { serialize as facultySerialize } from '../faculty';
+import { MajorModel, StudentModel, createMajorModel, createStudentModel } from '../model';
 
 export const serialize = (major:Document) => ({
   id: major.id,
   code: major.get('code'),
   name: major.get('name'),
-  faculty: major.get('faculty') instanceof Model ? facultySerialize(major.get('faculty')) : major.get('faculty'),
 });
 
 export class MajorController {
 
   constructor(
-    private facultyModel: FacultyModel = createFacultyModel(),
     private majorModel: MajorModel = createMajorModel(),
     private studentModel: StudentModel = createStudentModel(),
   ) {
     this.findAll = this.findAll.bind(this);
     this.findById = this.findById.bind(this);
-    this.findAllByFaculty = this.findAllByFaculty.bind(this);
     this.findByStudent = this.findByStudent.bind(this);
   }
 
   async findAll(req: Request, res: Response, next: NextFunction) {
-    const majors = await this.majorModel.find().populate('faculty').exec();
+    const majors = await this.majorModel.find();
     res.json(majors.map(serialize));
   }
 
   async findById(req: Request, res: Response) {
     let major = undefined;
     if (Types.ObjectId.isValid(req.params['id'] || '')) {
-      major = await this.majorModel.findById(req.params['id'] || '').populate('faculty').exec();
+      major = await this.majorModel.findById(req.params['id'] || '').exec();
     }
     if (major === null || major === undefined) {
       return res.status(404).json({'code': 400, 'message': 'not found'});
@@ -41,36 +36,21 @@ export class MajorController {
     res.json(serialize(major));
   }
 
-  async findAllByFaculty(req:Request, res: Response) {
-    let faculty = undefined;
-    if (Types.ObjectId.isValid(req.params['facultyId'] || '')) {
-      faculty = await this.facultyModel.findById(req.params['facultyId'] || '').exec();
-    }
-    if (faculty === null || faculty === undefined) {
-      return res.status(404).json({'code': 400, 'message': 'not found'});
-    }
-    const majors = await this.majorModel.find({faculty: faculty.id}).exec();
-    majors.map(major => major.set('faculty', faculty));
-    res.json(majors.map(serialize));
-  }
-
   async findByStudent(req: Request, res: Response) {
     let student = undefined;
     if (Types.ObjectId.isValid(req.params['studentId'] || '')) {
-      student = await this.studentModel.findById(req.params['studentId'] || '').populate('major').populate('faculty').exec();
+      student = await this.studentModel.findById(req.params['studentId'] || '').populate('majors').exec();
     }
     if (student === null || student === undefined || student.major === null || student.major === undefined) {
       return res.status(404).json({'code': 400, 'message': 'not found'});
     }
-    student.major.faculty = student.faculty;
-    res.json(serialize(student.major));
+    res.json(student.majors.map(serialize));
   }
 
   getRouter(): Router {
     const router = Router();
     router.get('/majors', this.findAll.bind(this));
     router.get('/majors/:id', this.findById.bind(this));
-    router.get('/faculties/:facultyId/majors', this.findAllByFaculty.bind(this));
     router.get('/students/:studentId/major', this.findByStudent.bind(this));
     return router;
   }
